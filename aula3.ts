f91bea467e5a9bc007bab699cc4260cae2a3a70f
Líria Li4 <lirialia51@gmail.com>
2023-03-21 11:21:11 +0100

    objectos em typeScript

---
+++ aula3.ts
@@ -0,0 +1,10 @@
+let objecto : {name: string, idade: number, programadora?: boolean} = {
+    name: 'Líria Li4',
+    idade: 25
+}
+
+let objecto2 : {[key:string]: any} = {
+    name: 'Líria Li4',
+    idade: 25,
+    programadora: true
+}
